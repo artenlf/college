ff5ccee7de1ba6f4cbda36d8eb2ae3c671724e0c
arten <felipe.arten@gmail.com>
2023-08-26 17:41:52 -0300

    unit 4 exercises

--- 01-Algoritmos e Programação Estruturada/Classes/data structures/lists/node-struct.c
+++ 01-Algoritmos e Programação Estruturada/Classes/data structures/lists/node-struct.c
@@ -6,10 +6,12 @@
 struct Node {
   int info;
   struct Node* next;
+  struct Node* prev;
 };
 
 struct List {
   struct Node* init;
+  struct Node* end;
   int length;
 };
 
@@ -17,6 +19,7 @@ struct List* create() {
   struct List* new_list = (struct List*) malloc(sizeof(struct List));
   if (new_list != NULL) {
     new_list->init = NULL;
+    new_list->end = NULL;
     new_list->length = 0;
   }
 
@@ -40,19 +43,44 @@ void insert(struct List* list, int position, int item) {
   new_node->info = item;
   
   if (position == 0) {
+    new_node->prev = list->end;
     new_node->next = list->init;
     list->init = new_node;
+    if (list->end == NULL) {
+      list->end = new_node;
+    }
+  } else if (position == list->length) {
+    new_node->prev = list->end;
+    new_node->next = NULL;
+    list->end->next = new_node;
+    list->end = new_node;
   } else {
     struct Node* aux = list->init;
     for (int i = 0; i < position - 1; i++) {
       aux = aux->next;
     }
 
+    new_node->prev = aux;
     new_node->next = aux->next;
     aux->next = new_node;
   }
 
   list->length++;
+}
+
+int length(struct List* list) {
+  assert(list != NULL);
+  return list->length;
+}
+
+void print(struct List* list) {
+  assert(list != NULL);
+  printf("\nList: ");
+  struct Node* aux = list->init;
+  for(int i = 0; i < list->length; i++) {
+    printf("%d ", aux->info);
+    aux = aux->next;
+  }
 }
 
 int read(struct List* list, int position) {
@@ -83,39 +111,43 @@ int remove_item(struct List* list, int position) {
   assert(empty(list) == false);
   assert(position >= 0 && position < list->length);
 
-  struct Node* previous = NULL;
-  struct Node* aux = list->init;
-  for(int i = 0; i < position; i++) {
-    previous = aux;
-    aux = aux->next;
-  }
+  struct Node* aux = NULL;
 
-  if (previous == NULL) {
+  if (position == 0) {
+    aux = list->init;
     list->init = aux->next;
+    if (list->init == NULL) {
+      list->end = NULL;
+    } else {
+      list->init->prev = NULL;
+    } 
+  } else if (position == list->length - 1) {
+    aux = list->end;
+    list->end = aux->prev;
+    list->end->next = NULL;
   } else {
-    previous->next = aux->next;
-  }
+    struct Node* previousNode = NULL;
+    aux = list->init;
 
-  int item = aux->info;
-  list->length--;
-  free(aux);
+    for (int i = 0; i < position; i++) {
+      previousNode->next = aux->next;
+      aux->next->prev = previousNode;
+    }
 
-  return item;
-}
+    int item = aux->info;
+    list->length--;
+    
+    free(aux);
 
-void print(struct List* list) {
-  assert(list != NULL);
-  printf("\nList: ");
-  struct Node* aux = list->init;
-  for(int i = 0; i < list->length; i++) {
-    printf("%d ", aux->info);
-    aux = aux->next;
+    return item;
   }
 }
 
-int length(struct List* list) {
-  assert(list != NULL);
-  return list->length;
+void freeMemoryList(struct List* list) {
+  while(empty(list) == false) {
+    remove_item(list, 0);
+  }
+  free(list);
 }
 
 int main() {
@@ -149,12 +181,7 @@ int main() {
   printf("\nRemoved value from position 2: %d\n", removed_value);
   print(my_list);
 
-  while (my_list->init != NULL) {
-    struct Node* temp = my_list->init;
-    my_list->init = my_list->init->next;
-    free(temp);
-  }
-  free(my_list);
+  freeMemoryList(my_list);
 
   return 0;
 }
